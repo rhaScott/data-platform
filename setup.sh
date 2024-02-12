08b021ad43c7855e28be5015c97976835479756a
Robert Scott <robert.h.a.scott@gmail.com>
2024-02-12 17:43:58 +0100

    update setup

--- setup.sh
+++ setup.sh
@@ -31,6 +31,43 @@ gcloud iam workload-identity-pools providers describe "data-platform" \
   --workload-identity-pool="github" \
   --format="value(name)"
 
+## Creating a service account to impersonate with the workload identity pool
+gcloud iam service-accounts create data-platform \
+  --description="Service account for mock data-platform" \
+  --display-name="data-platform"
+
+gcloud iam service-accounts list --filter="displayName:'data-platform'" --format='value(email)'
+
+#Enable IAM credentials API
+gcloud services enable iamcredentials.googleapis.com --project=336697166112
+
+#Some permissions for the service account
+gcloud projects add-iam-policy-binding data-platform-413021 \
+  --member='serviceAccount:data-platform@data-platform-413021.iam.gserviceaccount.com' \
+  --role='roles/bigquery.jobUser'
+
+gcloud projects add-iam-policy-binding data-platform-413021 \
+  --member='serviceAccount:data-platform@data-platform-413021.iam.gserviceaccount.com' \
+  --role='roles/iam.serviceAccountTokenCreator'
+
+gcloud projects add-iam-policy-binding data-platform-413021 \
+  --member='serviceAccount:data-platform@data-platform-413021.iam.gserviceaccount.com' \
+  --role='roles/iam.serviceAccountUser'
+
+# This command is a little hazy, I ran it after things started working. 
+# Going into https://console.cloud.google.com/iam-admin/workload-identity-pools/pool/github and granting access with repository_owner also worked.
+  gcloud iam service-accounts add-iam-policy-binding data-platform@data-platform-413021.iam.gserviceaccount.com \
+    --role='roles/iam.workloadIdentityUser' \
+    --member="principal://iam.googleapis.com/projects/336697166112/locations/global/workloadIdentityPools/github/subject/github"
+
+gcloud projects add-iam-policy-binding data-platform-413021 \
+  --member='serviceAccount:data-platform@data-platform-413021.iam.gserviceaccount.com' \
+  --role='roles/storage.objectCreator'
+
+gsutil mb -p data-platform-413021 -l EU gs://data-platform/
+
+# Below here hasn't been run. 
+################################
 ## Secret Manager Setup
 # Enable Secret Manager
 gcloud services enable secretmanager.googleapis.com --project="${PROJECT_ID}"
